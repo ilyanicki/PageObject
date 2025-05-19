dd77bbfb07ee1ca0d8150a1e00f58020d141b671
ilyanicki <genkagnab@gmail.com>
2025-05-19 14:15:36 +0400

    добавил генерацию отчёта allure

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 Это учебный проект по автомазиции тестирования с применением паттерна Page Object Model
 
 Для запуска тестов и генерации отчёта выполнить команду 
-pytest -v --alluredir=allure-results && allure serve allure-results
+rm -rf allure-results/
+pytest -v --alluredir=allure-results 
+allure serve allure-results
--- test_product_page.py
+++ test_product_page.py
@@ -5,7 +5,7 @@ from .pages.product_page import ProductPage
 from .pages.basket_page import BasketPage
 
 @pytest.mark.need_review
-@pytest.mark.parametrize('path', ['0', '1', '2', '3', '4', '5', '6', pytest.param("7", marks=pytest.mark.xfail), '8','9'])
+@pytest.mark.parametrize('path', ['6', pytest.param("7", marks=pytest.mark.xfail), '8','9'])
 def test_quest_can_add_product_to_basket(browser, path):
     browser.implicitly_wait(10)
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer" + path
@@ -35,7 +35,7 @@ def test_guest_cant_see_success_message(browser):
     page.open()
     page.should_not_be_success_message()
 
-@pytest.mark.xfail
+#@pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
     browser.implicitly_wait(10)
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer"
